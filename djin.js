c26c23c0a9b660b327c6864aecd773268da20ff9
nikolai <mail@nikolai.fyi>
2022-06-03 20:54:46 -0400

    improov

--- djin.js
+++ djin.js
@@ -1,23 +1,15 @@
 // engine
 
 class Djin {
-    let _tree
-    let desk
-    constructor(datadesk) {
-        _tree = datadesk
+    constructor(tree) {
+        this.tree = tree
+        this.nonc = 0
     }
-    // override
-    // desk is a purely functional set handle mutable handle
-    // you have access to _tree if you want to switch branches
-    // just load, modify, and return the new system state you want
-    // without fear
-    turn(desk, mail) {
-        throw new Error(`unimplemented override`)
-    }
-    // called by dmon
-    _turn(mail) {
-        let [desk, outs] = turn(this.desk, mail)
-        this.desk = _tree.save(desk)
+    turn(mail) {
+        let outs = []
+        this.tree.edit(this.nonc, this.nonc++, db => {
+            // do stuff
+        })
         return outs
     }
 }
--- dmon.js
+++ dmon.js
@@ -1,20 +1,36 @@
 // daemon
 
-// new Dmon(cash.djin, http)
-// dmon.task(cash.sync)
-// await dmon.serve()
-
 class Dmon {
     constructor(djin, plug) {
         this.djin = djin
         this.plug = plug
+        this.log = []
+        this.iq = []
+        this.oq = []
     }
-    serve() {
+    step() {
+        let mail = iq.pop()
+        let outs = this.djin.turn(mail)
+        this.oq = [...outs, ...this.oq]
+    }
+    async *spin() {
+        if (this.iq.length > 0) {
+            this.step()
+        } else {
+            yield
+        }
+    }
+    async play() {
         this.plug.when(mail => {
-            this.djin._turn(mail)
+            this.log.push(['i', mail])
+            this.iq = [mail, ...this.iq]
         })
-    }
-    async task(f) {
-        while (await f()) {}
+        setInterval(() => {
+            let outs = this.oq; this.oq = []; // flush
+            for (let out of outs) {
+                this.plug.send(out)
+            }
+        }, 1000)
+        spin()
     }
 }
